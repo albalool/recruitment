4e4ac3b337ba7f8bd8c215550f9b713de4197aa3
Lateefa Albalool <albalool.sec.com>
2022-09-27 20:37:33 +0300

    Solved except main

--- recruitment.py
+++ recruitment.py
@@ -2,40 +2,64 @@
 # This is the list that the user will choose from
 # Add at least 3 random skills for the user to select from
 def get_skills():
-    ...
+    skills = ["Python" , "C++" , "Javascript"]
+    return skills
 
+#print(get_skills())
 
 # This function pretty prints the skills to the user
 # It takes the list of skills as an argument and prints them numbered
 # This function doesn't return anything
-def show_skills(skills):
-    ...
 
+skills = get_skills()
+def show_skills(skills):
+    for skill in skills:
+        print(skill)   
+
+#show_skills(skills)
 
 # Shows the available skills and have user pick from them two skills
 # HINT: Use previous built functions to show the skills
 # For example, if the user enters 1, the first skill in your list of skills will be added to the list
 # Return a list of the two skills that the user inputted
 def get_user_skills(skills):
-    ...
+    show_skills(skills)
+    skill1 = input('Choose skill: ')
+    skill2 = input('Choose another skill: ')
+    lst = [skill1, skill2]
+    return lst
+
+# print(get_user_skills(skills))
 
 
 # This function will get the user's cv from their inputs
 # HINT: Use previous built functions to get the skills from the user
+cv = {}
 def get_user_cv(skills):
-    ...
+    name = input('What is your name: ')
+    age = int(input('What is your age: '))
+    experience = int(input('What is your years experience: '))
+    cv = {
+        "name": name,
+        "age": age,
+        "experince": experience,
+        "skills": get_user_skills(skills)
+    }
+    return cv
 
 
 # This functions checks if the cv is acceptable or not, by checking the age, experience and skills and return a boolean (True or False) based on that
 def check_acceptance(cv, desired_skill):
-    ...
+    if (25 >= cv["age"] <= 40 and cv["experince"] > 3 and desired_skill in cv["skills"]):
+        return True
 
+# print(check_acceptance(get_user_cv(skills),"Python"))
 
 def main():
-    # Write your main logic here by combining the functions above into the
-    # desired outcome
-    ...
-
+#     print("Welcome to the special recruitment program, please answer the following questions:")
+#     print(get_user_cv(skills))
+#  #   print(check_acceptance(cv,"Python"))
+#     print("You have been accepted", cv["name"])
 
 if __name__ == "__main__":
     main()
